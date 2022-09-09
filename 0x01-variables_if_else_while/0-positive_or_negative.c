f4e5c0f9362a4d8064f88171286e758099b19a2c
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 06:10:23 -0700

    0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,26 +7,28 @@
  *
  * Return: Always 0 (Success/correct)
  */
+
 int main(void)
 {
+int n;
 
-	int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
 
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 
-	return(0);
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
 
-
+return(0);
 }
