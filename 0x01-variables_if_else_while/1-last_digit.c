49480b37fedc4a5e7cc7d65566658286266f804a
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 12:11:08 -0700

    1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ else if (l == 0)
 
 else 
 {
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, );
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 
 return (0);
