8b99118e77f81fc56869375904414cbdceac2b08
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 16:45:23 -0700

    100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,25 @@
 
 int main(void)
 {
-	int num_1, num_2;
+int num_1, num_2;
 
-	for (num_1 = 0; num_1 < 9; num_1++)
-	{
-		for (num_2 = num_1 + 1; num_2 < 10; num_2++)
-	{
-			putchar ((num_1 % 10) + '0');
-			putchar ((num_2 % 10) + '0');
+for (num_1 = 0; num_1 < 9; num_1++)
+{
+	for (num_2 = num_1 + 1; num_2 < 10; num_2++)
+{
+		putchar ((num_1 % 10) + '0');
+		putchar ((num_2 % 10) + '0');
 
-			if (num_1 == 8 && num_2 == 9)
-			continue;
+		if (num_1 == 8 && num_2 == 9)
+		continue;
 
-			putchar(',');
+		putchar(',');
 
-			putchar(' ');	
-		}
+		putchar(' ');	
 	}
+}
 
-	putchar ('\n');
+putchar ('\n');
 
-	return (0);
+return (0);
 }
