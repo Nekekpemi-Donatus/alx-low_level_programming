5fbb510b843f3d8fe542f1ad7a5b33ef13c40eb3
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 18:58:27 -0700

    102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int num_1, num_2;
 	for (num_1 = 0; num_1 < 100; num_1++)
+
 	{
 		for (num_2 = 0; num_2 < 100; num_2++)
+
 		{
 			if (num_2 > num_1)
+
 			{
 				putchar ((num_1 / 10) + '0');
 				putchar ((num_1 % 10) + '0');
@@ -21,6 +22,7 @@ int main(void)
 				putchar ((num_2 / 10) + '0');
 				putchar ((num_2 % 10) + '0');
 				if (num_1 != 98)
+
 				{
 					putchar(',');
 					putchar(' ');
