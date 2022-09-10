df80344b9641667d3b7e71dc0b4bb3f4bbe89b69
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 18:05:50 -0700

    102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,29 +10,28 @@
 
 int main(void)
 {
-int num_1, num_2;
+	int num_1, num_2;
 
-for (num_1 = 0; num_1 < 100; num_1++)
-{
-	for (num_2 = num_1 + 1; num_2 < 100; num_2++)
+	for (num_1 = 0; num_1 < 100; num_1++)
 	{
-		putchar ((num_1 / 10) + '0');
-		putchar ((num_1 % 10) + '0');
-		putchar (' ');
-		putchar ((num_2 / 10) + '0');
-		putchar ((num_2 % 10) + '0');
+		for (num_2 = num_1 + 1; num_2 < 100; num_2++)
+		{
+			putchar ((num_1 / 10) + '0');
+			putchar ((num_1 % 10) + '0');
+			putchar (' ');
+			putchar ((num_2 / 10) + '0');
+			putchar ((num_2 % 10) + '0');
 
-		if (num_1 == 98 && num_2 == 99)
-			continue;
+			if (num_1 == 98 && num_2 == 99)
+				continue;
 
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 
+		}
 	}
-}
 }
 
 putchar ('\n');
-
 return (0);
 }
