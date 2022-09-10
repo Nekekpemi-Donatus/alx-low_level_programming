4ac59f29af84f297da2d1e4452f90330e7506929
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 18:47:48 -0700

    102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  *
  *Write a program that prints all possible combinations of two two-digit numbers.
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
