00bd27f2d62dcbbb6cdd6f6fcbeab76cc730fd5a
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-09 19:06:41 -0700

    102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 int main(void)
 {
-	int num_1, num_2;
-	for (num_1 = 0; num_1 < 100; num_1++)
+    int i, j;
+    int a, b, c, d;
 
-	{
-		for (num_2 = 0; num_2 < 100; num_2++)
+    for (i = 0; i < 100; i++)
+    {
+        a = i / 10; /* doubles fnum */
+        b = i % 10; /* singles fnum */
 
-		{
-			if (num_2 > num_1)
+        for (j = 0; j < 100; j++)
+        {
+            c = j / 10; /* doubles snum */
+            d = j % 10; /* singles snum */
 
-			{
-				putchar ((num_1 / 10) + '0');
-				putchar ((num_1 % 10) + '0');
-				putchar (' ');
-				putchar ((num_2 / 10) + '0');
-				putchar ((num_2 % 10) + '0');
-				if (num_1 != 98)
+            if (a < c || (a == c && b < d))
+            {
+                putchar(a + '0');
+                putchar(b + '0');
+                putchar(32);
+                putchar(c + '0');
+                putchar(d + '0');
 
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar ('\n');
-	return (0);
+                if (!(a == 9 && b == 8))
+                {
+                    putchar(44);
+                    putchar(32);
+                }
+            }
+        }
+    }
+    putchar(10);
+
+    return (0);
 }
-
