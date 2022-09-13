1295fbd5d366d916c0b1ce90248d85c6ad847d9e
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-13 11:33:23 -0700

    8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * jack_bauer - function that prints every minute of the day of Jack Baue, start
  * from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and reset mins
- * Return: Always 0 (Success)
+ * Return: Always 0 
  */
-
 void jack_bauer(void)
 {
 	int hours = 0;
