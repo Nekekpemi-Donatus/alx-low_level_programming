1e66831e7bc7e1bcff7f6d5ebd450242aac5f871
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-15 06:36:58 -0700

    first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * 0-isupper.c - function that checks for uppercase character.
+ * _isupper - function that checks for uppercase character.
  * @c: character to be tested
  * Reture: 1 whether it is, 0 otherwise
  */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
