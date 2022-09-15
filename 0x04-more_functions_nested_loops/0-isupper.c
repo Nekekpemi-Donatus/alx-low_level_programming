3c9b5a0946ba659f419c1ffa94ca4535605f3353
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-15 06:32:34 -0700

    0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,13 +5,10 @@
  * @c: character to be tested
  * Reture: 1 whether it is, 0 otherwise
  */
-
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
-	{
 		return (1);
-	}
 	else
 		return (0);
 }
