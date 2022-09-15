cff900257486956d7360498f31691239f0a0c96b
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-15 07:22:15 -0700

    first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,12 @@
  * @c: character to be tested
  * Reture: 1 whether it is, 0 otherwise
  */
+
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	else
-		return (0);
+	return (0);
 }
