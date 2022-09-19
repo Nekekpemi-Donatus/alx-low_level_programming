a4563c32c3c55c8709cfe625e75ece7f3d1915f0
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-09-19 07:45:56 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string of length
+ * Return: return length as integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		i++;
+	}
+	return(len);
+}
