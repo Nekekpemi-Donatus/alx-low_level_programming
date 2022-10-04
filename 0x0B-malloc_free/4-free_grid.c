38e321a5093c4090d75e83ed15d9ca702ccc5c31
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-10-04 16:23:53 -0700

    commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * free_grid - Frees a 2D array.
- * @grid: Array.
- * @height: height of the array
- * Return: return void
+ * free_grid - frees a 2 dimensional grid.
+ * @grid: multidimensional array of integers.
+ * @height: height of the grid.
+ *
+ * Return: no return
  */
 void free_grid(int **grid, int height)
 {
-	int i;
-
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-	free(grid);
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }
