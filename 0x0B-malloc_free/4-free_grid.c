c1af3eb12c64a2b2cfc2b9bdbfdebde68705f906
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-10-04 05:22:46 -0700

    commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - Frees a 2D array.
+ * @grid: Array.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
